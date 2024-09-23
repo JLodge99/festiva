8aa47cfc375eb0216e24c03d5ee08eceb1414ce6
JLodge99 <jrld1999@gmail.com>
2024-09-23 19:56:03 -0400

    Shadcn examples

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1,10 +1,15 @@
 import { Button } from './components/ui/button';
+import { Dashboard } from './components/ui/dashboard';
+import { TooltipProvider } from './components/ui/tooltip';
 
 export function App() {
 	return (
 		<div>
-			<Button variant="default">Save</Button>
-			<Button variant="destructive">Delete</Button>
+			<TooltipProvider>
+				<Dashboard />
+				<Button variant="default">Save</Button>
+				<Button variant="destructive">Delete</Button>
+			</TooltipProvider>
 		</div>
 	);
 }
